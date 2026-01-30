1987f8f6f30131dee0eff531e031b84cbe4a4c15
Douglas <ghostbrasilto@gmail.com>
2026-01-29 23:55:43 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('button').forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert(`Button ${btn.textContent} clicked!`);
+  });
+});
+
+console.log('Script loaded and event listeners attached.');
+console.log('Additional log for debugging purposes.');
+console.log ("This is a new log statement added to the script.");
+console.log("Estudo de git e github.");
+    
